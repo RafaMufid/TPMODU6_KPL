5858e6952673f453974de673f81b922c4cc59882
RafaMufid <akurafamufid@gmail.com>
2025-03-23 22:42:08 +0700

    menambahkan class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random randomId = new Random();
+        this.id = randomId.Next(10000, 99999);
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        this.playCount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("ID: " + this.id);
+        Console.WriteLine("Title: " + this.title);
+        Console.WriteLine("Play Count: " + this.playCount);
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design by Contract - Rafa Mufid 'Aqila");
+        video.PrintVideoDetails();
+    }
+}
